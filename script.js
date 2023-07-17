fdf079ec6e1475a6b34138a42c72c2c0c83335a3
bdeitr <132741187+bdeitr@users.noreply.github.com>
2023-07-17 16:21:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
 
-
+/*
 //per DIV INFORMAZIONI PROGETTI
 $(document).ready(function(){
-  $( ".info" ).hover(function(n) {
+  $( ".info" ).on("mouseenter", function(n) {
   $(this).next().toggle();
 } );
 });
 
+$(document).ready(function(){
+  $( ".info" ).on("mouseleave", function(n) {
+  $(this).next().toggle();
+} );
+});
 
+$(document).ready(function(){
+  $( ".info" ).on("click", function(n) {
+  $( "project" ).toggle();
+} );
+});
+
+function OpenMore(n) {
+    var foto = $(".project");
+    foto[n].style.display = "block";
+    
+    //var foto = $(".info");
+    //foto[n].style.display = "none";
+$( this ).find( "gallery" ).last().toggle();
+$(this).style.backgroundColor= "#646464";
+
+}
+
+    //For multiple modals
+    var more = $(".project");
+    //i represents which modal. It will go through all modals
+    for (var i = 0; i < more.length; i++) {
+    }
+
+*/
+
+function OpenMore(n) {
+  var info = $(".project");
+   // info[n].style.display = "block";
+    if (info[n].style.display = "none") {info[n].style.display = "block"};
+};
+
+
+/*
+var info = $(".project")
+$(".riga").click(function(n){
+  info.toggle();
+});*/
